2a881bbcaddd732a640bd2fd074ef66e2936319d
morten <938091+mortefos@users.noreply.github.com>
2019-04-15 18:30:20 +0200

    created separate history object
    for use in components and hooks

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,35 +1,47 @@
 import React from "react";
 import {
-  HashRouter as Router,
+  Router,
   Route,
   Switch,
-  RouteProps
+  RouteProps,
+  RouteComponentProps
 } from "react-router-dom";
 
 import PrivateRoute from "./private-route";
 import LoginScreen from "../user/login-screen";
-import CharacterList from "../characters/character-list";
-import CharacterSheet from "../characters/character-sheet";
 import { PoeProvider } from "../poe-content";
 import { AppToolBar } from "../components";
-import CharacterProvider from "../characters/character.provider";
+import {
+  CharacterSheet,
+  CharacterList,
+  CharacterProvider
+} from "../characters";
+import history from "./history";
 
 export interface RouterProps extends RouteProps {
   isAuthenticated: boolean;
 }
 
-const Providers: React.FunctionComponent = ({ children }) => (
-  <PoeProvider>
-    <CharacterProvider>{children}</CharacterProvider>
-  </PoeProvider>
+const Providers: React.FunctionComponent = ({ children, ...props }) => (
+  <PoeProvider>{children}</PoeProvider>
 );
 
 const AppRouter: React.FunctionComponent<any> = () => (
-  <Router>
+  <Router history={history}>
     <Providers>
       <AppToolBar />
       <Switch>
-        <PrivateRoute path="/character/:name" component={CharacterSheet} />
+        <PrivateRoute
+          path="/character/:name"
+          component={({ match: { params } }: RouteComponentProps) => {
+            const { name } = params as any; //TODO: figure out how to set up interfaces for params
+            return (
+              <CharacterProvider characterName={name}>
+                <CharacterSheet />
+              </CharacterProvider>
+            );
+          }}
+        />
         <PrivateRoute exact path="/" component={CharacterList} />
         <Route path="/login" component={LoginScreen} />
       </Switch>
